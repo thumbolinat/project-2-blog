cccb8e9cd5b31239275effc9998c34e179cdd34c
thumbolinat <93739066+thumbolinat@users.noreply.github.com>
2022-02-26 17:49:57 -0500

    updated db

--- config/connection.js
+++ config/connection.js
@@ -1,28 +1,15 @@
 // import the Sequelize
 const Sequelize = require('sequelize');
-
 require('dotenv').config();
-
 // create connection to our database, pass in your MySQL information for username and password
-
 // let sequelize;
-
 // if (process.env.JAWSDB_URL) {
 //     sequelize = new Sequelize(process.env.JAWSDB_URL);
 //   } else {
-<<<<<<< HEAD
-   const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-=======
     const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
->>>>>>> 5eaa381e9684207ea37c58f6dac203d3f5346b23
       host: 'localhost',
       dialect: 'mysql',
       port: 3306
     });
-<<<<<<< HEAD
-// }
-=======
 //}
->>>>>>> 5eaa381e9684207ea37c58f6dac203d3f5346b23
-
 module.exports = sequelize;
--- db/schema.sql
+++ db/schema.sql
@@ -2,9 +2,5 @@ DROP DATABASE IF EXISTS project_2_blog;
 
 CREATE DATABASE project_2_blog;
 
-<<<<<<< HEAD
-CREATE DATABASE blog_db;
 
-USE blog_db;
-=======
->>>>>>> 5eaa381e9684207ea37c58f6dac203d3f5346b23
+
