dbace05a5cea00604abaf6e2be328c6f185079ab
Vithursan6 <93847590+Vithursan6@users.noreply.github.com>
2022-02-27 16:04:31 -0500

    initial deployment

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,14 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 // create connection to our database, pass in your MySQL information for username and password
-// let sequelize;
-// if (process.env.JAWSDB_URL) {
-//     sequelize = new Sequelize(process.env.JAWSDB_URL);
-//   } else {
+let sequelize;
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+  } else {
     const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
       port: 3306
     });
-//}
+}
 module.exports = sequelize;
