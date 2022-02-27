059babdb5763b7e022c7428c15cc205c443f85ee
Jaimit Patel <jaimit_12@hotmail.com>
2022-02-27 05:43:18 -0500

    updated dashboard-routes with the one on main

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,12 +1,15 @@
-const sequelize = require('../config/connection');
 const router = require('express').Router();
+const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
+// checks if user logged in; have session; authorized
 const withAuth = require('../utils/auth');
 
-router.get('/', withAuth, (req, res) => { 
+// all dashboard views will be prefixed with /dashboard
+router.get('/', withAuth, (req, res) => { // add withAuth here as our own middlware
     Post.findAll({
         where: {
+          // use the ID from the session so we retrieve posts made by logged in user
           user_id: req.session.user_id
           
         },
@@ -25,8 +28,10 @@ router.get('/', withAuth, (req, res) => {
         ]
       })
         .then(dbPostData => {
-
+          // serialize data before passing to template
+          // using post here but this can be named ANYTHING 
           const posts = dbPostData.map(post => post.get({ plain: true }));
+          // user won't be able to get to the dashboard page unless they're logged in
           res.render('dashboard', { posts, loggedIn: true });
         })
         .catch(err => {
@@ -36,7 +41,7 @@ router.get('/', withAuth, (req, res) => {
         console.log(req.session.user_id);
   });
 
-router.get('/edit/:id', withAuth, (req, res) => {
+router.get('/edit/:id', withAuth, (req, res) => { // add withAuth here as our own middlware
   Post.findOne({
     where: {
         id: req.params.id
@@ -51,7 +56,7 @@ router.get('/edit/:id', withAuth, (req, res) => {
         {
             model: Comment,
             attributes: ['id', 'comment_text', 'user_id', 'created_at'],
-            
+            // also include the User model itself so it can attach the username to the comment
             include: {
                 model: User,
                 attributes: ['username']
@@ -65,19 +70,19 @@ router.get('/edit/:id', withAuth, (req, res) => {
   })
     .then(dbPostData => {
         if(!dbPostData) {
-           
+            // The 404 status code identifies a user error and will need a different request for a successful response.
             res.status(404).json({ message: 'No post with this id was found'});
             return;
         }
 
-      
+        // serialize the data with plain: true
         const post = dbPostData.get({ plain: true });
 
-        
+        // pass data to template
         res.render('edit-post', { 
           post,
           loggedIn: req.session.loggedIn
-        
+          // user will only see comments if logged in
         });
     })  
     .catch(err => {
