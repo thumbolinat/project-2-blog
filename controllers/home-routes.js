0486164b7adc5b0a5947cb2e68d1221a5081c044
Jaimit Patel <jaimit_12@hotmail.com>
2022-03-02 17:20:59 -0500

    home now redirects to login

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,46 +3,17 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
-  console.log(req.session);
-
-    Post.findAll({
-        attributes: [
-          'id',
-          'title',
-          'post_text',
-          'created_at',
-          [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-        ],
-        order: [['created_at', 'DESC']],
-        include: [
-          {
-            model: Comment,
-            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-            include: {
-              model: User,
-              attributes: ['username']
-            }
-          },
-          {
-            model: User,
-            attributes: ['username']
-          }
-        ]
-      })
-        .then(dbPostData => {
-          const posts = dbPostData.map(post => post.get({ plain: true }));
-          // pass post objects into the homepage template
-          res.render('homepage', { posts, loggedIn: req.session.loggedIn });
-        })
-        .catch(err => {
-          console.log(err);
-          res.status(500).json(err);
-        });
+  if (req.session.user ==null) {
+    res.redirect ('/login')
+  } else {
+  res.redirect('/homepage')
+  }  
+  return
   });
 
   router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
-      res.redirect('/');
+      res.redirect('/homepage');
       return;
     }
 
