6f5216d49224266c566de3edbf4976893a70224d
Vithursan6 <93847590+Vithursan6@users.noreply.github.com>
2022-03-02 21:57:27 -0500

    reverted to Develop

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -41,46 +41,5 @@ router.get('/', withAuth, (req, res) => { // add withAuth here as our own middlw
         console.log(req.session.user_id);
   });
 
-  router.get('/edit/:id', withAuth, (req, res) => {
-    Post.findByPk(req.params.id, {
-      attributes: [
-        'id',
-        'post_url',
-        'title',
-        'created_at',
-        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-      ],
-      include: [
-        {
-          model: Comment,
-          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-          include: {
-            model: User,
-            attributes: ['username']
-          }
-        },
-        {
-          model: User,
-          attributes: ['username']
-        }
-      ]
-    })
-      .then(dbPostData => {
-        if (dbPostData) {
-          const post = dbPostData.get({ plain: true });
-          
-          res.render('edit-post', {
-            post,
-            loggedIn: true
-          });
-        } else {
-          res.status(404).end();
-        }
-      })
-      .catch(err => {
-        res.status(500).json(err);
-      });
-  });
-
 
 module.exports = router;
