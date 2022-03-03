faef44f93000bb3f26e9f9fbafe2ee5e7f920de9
Vithursan6 <93847590+Vithursan6@users.noreply.github.com>
2022-03-02 20:08:39 -0500

    fix 6 and test

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -42,7 +42,7 @@ async function signupFormHandler(event) {
         if (response.ok) {
             document.location.replace('/');
         } else {
-            alert(response.statusText);
+            alert('Incorrect username or Password');
         }
     }
   }
