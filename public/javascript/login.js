92226211199462df55e6aefac1a336fdbf33b449
Jaimit Patel <jaimit_12@hotmail.com>
2022-03-02 19:54:11 -0500

    fixed issues with sign in

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -4,7 +4,7 @@ async function signupFormHandler(event) {
     const password = document.querySelector('#password-signup').value.trim();
 
     if (username && password) {
-        const response = await fetch('/api/users/login', {
+        const response = await fetch('/api/users', {
         method: 'post',
         body: JSON.stringify({
             username,
